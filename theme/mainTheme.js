5fc4635ebddf40020772fed3df93e6d97d60efd2
Tomek <tomekz@vp.pl>
2021-07-13 14:56:44 +0200

    Add a theme file to the project
    Add a theme file, add all the dataset colors

---
+++ theme/mainTheme.js
@@ -0,0 +1,14 @@
+const mainTheme = {
+  colors: {
+    color_AstronautBlue: '#003f5c',
+    color_Chambray: '#2f4b7c',
+    color_ButterflyBush: '#665191',
+    color_Tapestry: '#a05195',
+    color_Cranberry: '#d45087',
+    color_Carnation: '#f95d6a',
+    color_Coral: '#ff7c43',
+    color_WebOrange: '#ffa600',
+  },
+};
+
+export default mainTheme;
