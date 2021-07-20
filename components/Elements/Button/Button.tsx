c72c03e6724370a16ad61537ac0a54195ccb2395
Tomek <tomekz@vp.pl>
2021-07-20 15:40:00 +0200

    Add Button and Input components

---
+++ components/Elements/Button/Button.tsx
@@ -0,0 +1,5 @@
+import { StyledButton } from './styles';
+
+export default function Button({ children }) {
+  return <StyledButton type='button'>{children}</StyledButton>;
+}
---
+++ components/Elements/Input/Input.tsx
@@ -0,0 +1,5 @@
+import { StyledInput } from './styles';
+
+export default function Input({ children }) {
+  return <StyledInput>{children}</StyledInput>;
+}
