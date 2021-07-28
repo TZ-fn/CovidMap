570a4c840343f1c967ca784266a4201f017b9065
Tomek <tomekz@vp.pl>
2021-07-28 15:49:48 +0200

    Add more styling to the app

--- components/Layout/styles.tsx
+++ components/Layout/styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import mainTheme from 'theme/mainTheme';
 
 export const StyledWrapper = styled.div`
   height: 100%;
@@ -7,14 +8,9 @@ export const StyledWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  background: rgb(0, 63, 92);
-  background: linear-gradient(
-    130deg,
-    rgba(0, 63, 92, 0.15) 0%,
-    rgba(188, 80, 144, 0.15) 50%,
-    rgba(255, 166, 0, 0.15) 100%
-  );
+  background: ${mainTheme.themeColors.background};
+
   & > * {
-    border: 1px solid black;
+    border: 1px solid white;
   }
 `;
