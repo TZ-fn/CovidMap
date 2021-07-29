e33c03ca2cf1df726ce65bb5fd7e4997c8f4bc4d
Tomek <tomekz@vp.pl>
2021-07-29 16:10:58 +0200

    Add a MainLogo component for the app logo

---
+++ components/Elements/MainLogo/MainLogo.tsx
@@ -0,0 +1,5 @@
+import { StyledMainLogo } from './styles';
+
+export default function MainLogo() {
+  return <StyledMainLogo>Covid Map</StyledMainLogo>;
+}
---
+++ components/Elements/MainLogo/styles.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledMainLogo = styled.h1`
+  font-size: 4rem;
+  margin: 0.33em 2em 0.33em 0;
+`;
