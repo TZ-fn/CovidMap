99d1552db53e96b69db6b90b9abc438b574ba842
Tomek <tomekz@vp.pl>
2021-08-01 20:45:40 +0200

    Improve the Button components styling

--- components/Elements/Button/styles.tsx
+++ components/Elements/Button/styles.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import mainTheme from 'theme/mainTheme';
 
 export const StyledButton = styled.button`
+  box-size: border-box;
   padding: 1em 1.5em;
   color: ${mainTheme.themeColors.fontColor};
   border: 1px solid ${mainTheme.dataColors.Coral};
@@ -9,6 +10,10 @@ export const StyledButton = styled.button`
   border-radius: 2px;
 
   &:hover {
-    background-color: blue;
+    background-color: ${mainTheme.themeColors.background};
+  }
+
+  &:focus {
+    outline: 2px solid ${mainTheme.dataColors.Coral};
   }
 `;
