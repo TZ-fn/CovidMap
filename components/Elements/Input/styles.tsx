3d5dc716f576c01488298e3982a4e860a56ffa71
Tomek <tomekz@vp.pl>
2021-08-02 17:18:25 +0200

    Add a custom clear icon for the Input element

--- components/Elements/Input/styles.tsx
+++ components/Elements/Input/styles.tsx
@@ -1,6 +1,31 @@
 import styled from 'styled-components';
+import mainTheme from 'theme/mainTheme';
+import deleteIcon from 'public/icons/icons8-delete.svg';
 
 export const StyledInput = styled.input`
   font-size: 1.3rem;
   padding: 0.33em 0.25em;
+  border-radius: ${mainTheme.misc.borderRadius};
+  color: ${mainTheme.themeColors.fontColor};
+  background-color: ${mainTheme.themeColors.background};
+
+  &:focus {
+    outline: 2px solid ${mainTheme.dataColors.Coral};
+  }
+
+  &::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+    height: 1em;
+    width: 1em;
+    border-radius: 50em;
+    background: url(${deleteIcon.src}) no-repeat 50% 50%;
+    background-size: contain;
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  &:focus::-webkit-search-cancel-button {
+    opacity: 1;
+    pointer-events: all;
+  }
 `;
