76ffb2501c6f2ec38fb675ac94206f26fe517de3
Tomek <tomekz@vp.pl>
2021-08-03 15:57:49 +0200

    Change the Search icon to improve the SearchButton look

--- components/Elements/SearchButton/SearchButton.styles.tsx
+++ components/Elements/SearchButton/SearchButton.styles.tsx
@@ -2,5 +2,7 @@ import styled from 'styled-components';
 import Button from '../Button/Button';
 
 export const StyledSearchButton = styled(Button)`
-  padding: 0.5em 0.6em;
+  width: 3.65rem;
+  height: 3.65rem;
+  padding: 0.5em 0.6em 0em 0.6em;
 `;
