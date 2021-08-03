6575d6e4239da2514c8eb80712115f995aafdca6
Tomek <tomekz@vp.pl>
2021-08-03 15:57:49 +0200

    Change the Search icon to improve the SearchButton look

--- components/Elements/SearchButton/SearchButton.tsx
+++ components/Elements/SearchButton/SearchButton.tsx
@@ -1,5 +1,5 @@
 import { StyledSearchButton } from './SearchButton.styles';
-import SearchIcon from 'public/icons/iconmonstr-search-thin.svg';
+import SearchIcon from 'public/icons/icons8-search.svg';
 
 export default function SearchButton() {
   return (
