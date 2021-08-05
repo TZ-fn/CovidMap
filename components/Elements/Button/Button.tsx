5a664bff2028271c7a35bd2b605f697eaa361fd8
Tomek <tomekz@vp.pl>
2021-08-05 15:53:55 +0200

    Fix type errors in Button and Input components

--- components/Elements/Button/Button.tsx
+++ components/Elements/Button/Button.tsx
@@ -1,6 +1,11 @@
 import { StyledButton } from './Button.styles';
 
-export default function Button({ children, className }) {
+type ButtonProps = {
+  children: React.ReactNode;
+  className: string;
+};
+
+export default function Button({ children, className }: ButtonProps) {
   return (
     <StyledButton className={className} type='button'>
       {children}
--- components/Elements/Input/Input.tsx
+++ components/Elements/Input/Input.tsx
@@ -1,5 +1,10 @@
 import { StyledInput } from './Input.styles';
 
-export default function Input({ type, placeholder }) {
+type InputProps = {
+  type: string;
+  placeholder: string;
+};
+
+export default function Input({ type, placeholder }: InputProps) {
   return <StyledInput type={type} placeholder={placeholder} />;
 }
