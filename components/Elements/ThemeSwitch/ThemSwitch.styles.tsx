45de51cecb357c93a600dabef04b74a56514cb48
Tomek <tomekz@vp.pl>
2021-08-07 17:27:56 +0200

    Add icons to the ThemeSwitch component

---
+++ components/Elements/ThemeSwitch/ThemSwitch.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+import mainTheme from 'theme/mainTheme';
+import ToggleSwitch from 'components/Elements/ToggleSwitch/ToggleSwitch';
+
+export const StyledThemeSwitch = styled(ToggleSwitch)``;
--- components/Elements/ThemeSwitch/ThemeSwitch.tsx
+++ components/Elements/ThemeSwitch/ThemeSwitch.tsx
@@ -1,3 +1,14 @@
+import { StyledThemeSwitch } from 'components/Elements/ThemeSwitch/ThemSwitch.styles';
+import lightThemeIcon from 'public/icons/icons8-sun.svg';
+import darkThemeIcon from 'public/icons/reshot-icon-moon-crescent-UKRLZEWPYG.svg';
+
 export default function ThemeSwitch() {
-  return <input type='checkbox' />;
+  return (
+    <StyledThemeSwitch
+      name='theme-switch'
+      ariaLabel='Theme toggle'
+      leftLabel={lightThemeIcon.src}
+      rightLabel={darkThemeIcon.src}
+    />
+  );
 }
