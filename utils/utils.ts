e86575bb7807002937d477cc8cf930791df9d807
Tomek <tomekz@vp.pl>
2021-08-23 15:46:13 +0200

    Add custom utilities functions for handling the vaccine's doses data

--- utils/utils.ts
+++ utils/utils.ts
@@ -6,3 +6,17 @@ export async function fetchData(API: string) {
     return error;
   }
 }
+
+// Convert the country objects received from the API into tuples containing the country's name and number of vaccine doses administered, so they can be sorted more easily sorted
+
+function createCountriesTuples(countriesData) {
+  return countriesData.map((country) => {
+    return [country.country, Object.values(country.timeline)[0]];
+  });
+}
+
+export function sortCountriesByVaccineDoses(data, numberOfResults: number) {
+  return createCountriesTuples(data)
+    .sort((country1, country2) => country2[1] - country1[1])
+    .slice(0, numberOfResults);
+}
