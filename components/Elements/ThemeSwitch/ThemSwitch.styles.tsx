97825dbd77fac52f03f623171a9af669b45d1260
Tomek <tomekz@vp.pl>
2021-08-24 18:42:21 +0200

    Remove an obsolete import

--- components/Elements/ThemeSwitch/ThemSwitch.styles.tsx
+++ components/Elements/ThemeSwitch/ThemSwitch.styles.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import mainTheme from 'theme/mainTheme';
 import ToggleSwitch from 'components/Elements/ToggleSwitch/ToggleSwitch';
 
 export const StyledThemeSwitch = styled(ToggleSwitch)``;
