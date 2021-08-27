bdc3ec766fa48ea47d505c74570134d355c390fd
Tomek <tomekz@vp.pl>
2021-08-27 14:54:32 +0200

    Make useEffect synchronous, add a async function inside of it

--- components/Elements/List/List.tsx
+++ components/Elements/List/List.tsx
@@ -1,26 +1,32 @@
+import React, { ReactElement } from 'react';
 import CountriesListItem from 'components/CountriesListItem/CountriesListItem';
 import { useEffect, useState } from 'react';
 import { fetchData, sortCountriesByVaccineDoses } from 'utils/utils';
 import { StyledList } from './List.styles';
-
 type CountryType = [string, number];
 
-export default function List() {
+export default function List(): ReactElement {
   const [vaccinesData, setVaccinesData] = useState([]);
-  useEffect(async () => {
-    setVaccinesData(
-      await fetchData(
-        'https://disease.sh/v3/covid-19/vaccine/coverage/countries?lastdays=1&fullData=false',
-      ),
-    );
+
+  useEffect(() => {
+    async function getVaccinesData(): Promise<void> {
+      setVaccinesData(
+        await fetchData(
+          'https://disease.sh/v3/covid-19/vaccine/coverage/countries?lastdays=1&fullData=false',
+        ),
+      );
+    }
+
+    getVaccinesData();
   }, []);
 
   return (
     <StyledList>
-      {vaccinesData.length > 0
+      {vaccinesData?.length > 0
         ? sortCountriesByVaccineDoses(vaccinesData, 12).map((country: CountryType) => {
             return (
               <CountriesListItem
+                key={country[0]}
                 countryFlag='https://disease.sh/assets/img/flags/af.png'
                 countryName={country[0]}
                 numberOfCases={country[1]}
