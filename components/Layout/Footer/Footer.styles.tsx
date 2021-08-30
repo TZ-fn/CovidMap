0b7d82001166ed7ce28a6cbc0c2511323bca7c12
Tomek <tomekz@vp.pl>
2021-08-30 15:58:30 +0200

    Add and style the data source link

--- components/Layout/Footer/Footer.styles.tsx
+++ components/Layout/Footer/Footer.styles.tsx
@@ -3,11 +3,17 @@ import mainTheme from 'theme/mainTheme';
 
 export const StyledFooter = styled.footer`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   height: 7vh;
   width: 100%;
   background-color: ${mainTheme.themeColors.background};
   border-top: 1px solid ${mainTheme.themeColors.border};
+
+  & > a {
+    margin-left: 1rem;
+    text-decoration: underline;
+    text-underline-offset: 0.2rem;
+  }
 `;
--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -2,5 +2,12 @@ import React, { ReactElement } from 'react';
 import { StyledFooter } from './Footer.styles';
 
 export default function Footer(): ReactElement {
-  return <StyledFooter>Data source: Lorem ipsum dolor sit amet.</StyledFooter>;
+  return (
+    <StyledFooter>
+      Data source:
+      <a rel='noopener noreferrer' href='https://www.worldometers.info/coronavirus/'>
+        worldometers.info
+      </a>
+    </StyledFooter>
+  );
 }
