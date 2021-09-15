f095977e58e20dbf51729937cb012f6c5718133b
Tomek <tomekz@vp.pl>
2021-09-15 14:23:31 +0200

    Add more favicon options to ensure a better coverage

--- components/Layout/HeadElement/HeadElement.tsx
+++ components/Layout/HeadElement/HeadElement.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import MainLogo from '../../../public/Covid19_Map_Logo.svg';
 import capitalise from 'utils/capitalise.utils';
 
 interface HeadElementProps {
@@ -9,14 +8,18 @@ interface HeadElementProps {
 export default function HeadElement({ title = 'Covid-19 Map' }: HeadElementProps): JSX.Element {
   return (
     <Head>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+      <meta name='theme-color' content='#ffffff' />
       <meta charSet='utf-8' />
-      <link rel='icon' href={MainLogo.src} />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta
         name='description'
         content='Web application that displays Covid-19 data for different countries'
       />
-      <link rel='apple-touch-icon' href={MainLogo.src} />
       <title>{typeof title === 'string' && `${capitalise(title)} - Covid-19 Map`}</title>
       <meta name='author' content='Tomasz Ziółkowski' />
     </Head>
