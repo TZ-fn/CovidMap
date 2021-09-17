32c64f69bbc9c3d702ba16a66d8510bf8fe85847
Tomek <tomekz@vp.pl>
2021-09-17 15:33:10 +0200

    Remove the obsolete transition property from GlobalStyles

--- components/GlobalStyles/GlobalStyles.tsx
+++ components/GlobalStyles/GlobalStyles.tsx
@@ -4,7 +4,6 @@ const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.fontColor};
-    transition: all 0.50s linear;
   }
   `;
 
