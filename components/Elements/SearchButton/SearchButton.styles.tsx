f9741630d95770d96556e8f86603423330f87681
Tomek <tomekz@vp.pl>
2021-09-22 14:46:08 +0200

    Add the SearchButton icon svg inline, to make the theming it easier

--- components/Elements/SearchButton/SearchButton.styles.tsx
+++ components/Elements/SearchButton/SearchButton.styles.tsx
@@ -5,4 +5,8 @@ export const StyledSearchButton = styled(Button)`
   width: 57px;
   height: 57px;
   padding: 0.5em 0.6em 0em 0.6em;
+
+  & svg {
+    fill: ${({ theme }) => theme.fontColor};
+  }
 `;
