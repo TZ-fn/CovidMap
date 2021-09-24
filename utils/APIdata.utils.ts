04add3fc6629f23053a415cc9cb89dbdea7c93ae
Tomek <tomekz@vp.pl>
2021-09-24 15:49:32 +0200

    Refactor the sorting logic
    Both data types now use the same sorting function

--- utils/APIdata.utils.ts
+++ utils/APIdata.utils.ts
@@ -1,25 +1,15 @@
 import { VaccinesDataForCountry, CovidCasesDataForCountry } from './APIdata.types';
 
-export interface sortByVaccineDosesType {
-  (a: VaccinesDataForCountry, b: VaccinesDataForCountry): number;
-}
-
-export interface sortByCovidCasesType {
-  (a: CovidCasesDataForCountry, b: CovidCasesDataForCountry): number;
-}
-
-export const sortByVaccineDoses = (a: VaccinesDataForCountry, b: VaccinesDataForCountry): number =>
-  Object.values(b.timeline)[0] - Object.values(a.timeline)[0];
-
-export const sortByCovidCases = (
-  a: CovidCasesDataForCountry,
-  b: CovidCasesDataForCountry,
-): number => b.cases - a.cases;
-
 export function sortCountries(
-  data: VaccinesDataForCountry[] | CovidCasesDataForCountry[],
+  covidCasesData: CovidCasesDataForCountry[],
   numberOfResults: number,
-  sortBy: sortByVaccineDosesType | sortByCovidCasesType,
-): typeof data {
-  return data.sort((a, b) => sortBy(a, b)).slice(0, numberOfResults);
+  vaccinesData?: VaccinesDataForCountry[],
+): CovidCasesDataForCountry[] | VaccinesDataForCountry[] {
+  if (vaccinesData) {
+    return vaccinesData
+      .sort((a, b) => Object.values(b.timeline)[0] - Object.values(a.timeline)[0])
+      .slice(0, numberOfResults);
+  } else {
+    return covidCasesData.sort((a, b) => b.cases - a.cases).slice(0, numberOfResults);
+  }
 }
