6b35762ca4e05e223c5d5bb4f33ce248e7f71388
Tomek <tomekz@vp.pl>
2021-10-05 15:49:32 +0200

    Fix an error with countries' names
    Name of the countries used in the GeoJSON file were different than these used
    in the API, I've mad a function to convert the names that were different

---
+++ assets/data/countryNames.js
@@ -0,0 +1,23 @@
+const countryNamesFromCovidData = {
+  'Dominican Republic': 'Dominican Rep.',
+  'United States': 'USA',
+  'Falkland Is.': 'Falkland Islands (Malvinas)',
+  'United Arab Emirates': 'UEA',
+  'N. Cyprus': 'Cyprus',
+  Korea: 'S. Korea',
+  'Lao PDR': "Lao People's Democratic Republic",
+  Syria: 'Syrian Arab Republic',
+  'Central African Rep.': 'Central African Republic',
+  'Dem. Rep. Congo': 'Congo',
+  'Eq. Guinea': 'Equatorial Guinea',
+  Libya: 'Libyan Arab Jamahiriya',
+  'W. Sahara': 'Western Sahara',
+  'S. Sudan': 'South Sudan',
+  Somaliland: 'Somalia',
+  'Bosnia and Herz.': 'Bosnia',
+  'Czech Rep.': 'Czechia',
+  'United Kingdom': 'UK',
+  'Solomon Is.': 'Solomon Islands',
+};
+
+export default countryNamesFromCovidData;
--- components/Layout/MainMapContainer/Map/Map.tsx
+++ components/Layout/MainMapContainer/Map/Map.tsx
@@ -1,14 +1,62 @@
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import allCountries from 'public/geojsonData/allCountries.geo.json';
+import countryNamesFromCovidData from 'assets/data/countryNames';
+import theme from 'theme/theme';
+import { CovidCasesDataForCountry } from 'utils/APIdata.types';
 
-const Map = (): JSX.Element => {
-  const myStyle = {
-    color: 'hsl(198, 50%, 25%)',
-    weight: 1,
-    opacity: 0.5,
-    fillColor: '#66c2a5',
-    fillOpacity: 0.7,
+interface MapProps {
+  covidCasesData: CovidCasesDataForCountry[];
+}
+
+const Map = ({ covidCasesData }: MapProps): JSX.Element => {
+  const mapCountriesNamesToCovidDataCountries = (countryNameFromMap: string) => {
+    if (countryNamesFromCovidData[countryNameFromMap]) {
+      return countryNamesFromCovidData[countryNameFromMap];
+    } else {
+      return countryNameFromMap;
+    }
   };
+
+  const mapCountryToNumberOfCases = (countryNameFromMap: string) => {
+    return covidCasesData.filter(
+      (CovidCasesDataForCountry) =>
+        CovidCasesDataForCountry.country ===
+        mapCountriesNamesToCovidDataCountries(countryNameFromMap),
+    )[0]?.cases;
+  };
+
+  const mapNumberOfCasesToColor = (numberOfCases: number) => {
+    switch (true) {
+      case numberOfCases > 10000000:
+        return theme.dataColors.WebOrange;
+      case numberOfCases > 5000000:
+        return theme.dataColors.Coral;
+      case numberOfCases > 2500000:
+        return theme.dataColors.Carnation;
+      case numberOfCases > 1000000:
+        return theme.dataColors.Cranberry;
+      case numberOfCases > 100000:
+        return theme.dataColors.Tapestry;
+      case numberOfCases > 10000:
+        return theme.dataColors.ButterflyBush;
+      case numberOfCases > 1000:
+        return theme.dataColors.Chambray;
+      case numberOfCases > 100:
+        return theme.dataColors.AstronautBlue;
+      default:
+        return theme.dataColors.NoData;
+    }
+  };
+
+  function style(feature) {
+    return {
+      fillColor: mapNumberOfCasesToColor(mapCountryToNumberOfCases(feature.properties.name)),
+      weight: 0.7,
+      opacity: 0.7,
+      color: `${theme.darkTheme.background}`,
+      fillOpacity: 1,
+    };
+  }
 
   return (
     <MapContainer
@@ -21,7 +69,7 @@ const Map = (): JSX.Element => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <GeoJSON data={allCountries} style={myStyle} />
+      <GeoJSON data={allCountries} style={style} />
     </MapContainer>
   );
 };
