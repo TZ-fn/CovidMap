fc0dd5e2f813155ced851349421b680ec0d5c7a6
Tomek <tomekz@vp.pl>
2021-10-07 15:16:05 +0200

    Add a default shadow, remove obsolete semicolons from the shadows' strings

--- theme/theme.ts
+++ theme/theme.ts
@@ -38,13 +38,14 @@ export const theme = {
   misc: {
     borderRadius: '0.225em',
     shadows: {
+      default: '3px 3px 10px rgba(0, 0, 0, 0.3)',
       lowElevation: `
   0.7px 0.7px 2.2px rgba(0, 0, 0, 0.02),
   1.7px 1.7px 5.3px rgba(0, 0, 0, 0.028),
   3.1px 3.1px 10px rgba(0, 0, 0, 0.035),
   5.6px 5.6px 17.9px rgba(0, 0, 0, 0.042),
   10.4px 10.4px 33.4px rgba(0, 0, 0, 0.05),
-  25px 25px 80px rgba(0, 0, 0, 0.07);
+  25px 25px 80px rgba(0, 0, 0, 0.07)
 `,
       mediumElevation: `
   1.4px 1.4px 2.2px rgba(0, 0, 0, 0.02),
@@ -52,7 +53,7 @@ export const theme = {
   6.3px 6.3px 10px rgba(0, 0, 0, 0.035),
   11.2px 11.2px 17.9px rgba(0, 0, 0, 0.042),
   20.9px 20.9px 33.4px rgba(0, 0, 0, 0.05),
-  50px 50px 80px rgba(0, 0, 0, 0.07);
+  50px 50px 80px rgba(0, 0, 0, 0.07)
 `,
       highElevation: `
   2.1px 2.1px 2.2px rgba(0, 0, 0, 0.02),
@@ -60,7 +61,7 @@ export const theme = {
   9.4px 9.4px 10px rgba(0, 0, 0, 0.035),
   16.8px 16.8px 17.9px rgba(0, 0, 0, 0.042),
   31.3px 31.3px 33.4px rgba(0, 0, 0, 0.05),
-  75px 75px 80px rgba(0, 0, 0, 0.07);
+  75px 75px 80px rgba(0, 0, 0, 0.07)
 `,
     },
   },
