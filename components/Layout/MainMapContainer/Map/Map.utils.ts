74248620ad08b4575d724585e1b5c5716983b465
Tomek <tomekz@vp.pl>
2021-10-12 15:25:29 +0200

    Reverse the data set color palette

--- components/Layout/MainMapContainer/Map/Map.utils.ts
+++ components/Layout/MainMapContainer/Map/Map.utils.ts
@@ -23,22 +23,22 @@ export const mapCountryToNumberOfCases = (
 
 export const mapNumberOfCasesToColor = (numberOfCases: number, theme: ThemeType): string => {
   switch (true) {
+    case numberOfCases > 15000000:
+      return theme.dataColors.AstronautBlue;
     case numberOfCases > 10000000:
-      return theme.dataColors.WebOrange;
+      return theme.dataColors.Chambray;
     case numberOfCases > 5000000:
-      return theme.dataColors.Coral;
+      return theme.dataColors.ButterflyBush;
     case numberOfCases > 2500000:
-      return theme.dataColors.Carnation;
+      return theme.dataColors.Tapestry;
     case numberOfCases > 1000000:
       return theme.dataColors.Cranberry;
     case numberOfCases > 100000:
-      return theme.dataColors.Tapestry;
+      return theme.dataColors.Carnation;
     case numberOfCases > 10000:
-      return theme.dataColors.ButterflyBush;
-    case numberOfCases > 1000:
-      return theme.dataColors.Chambray;
-    case numberOfCases > 100:
-      return theme.dataColors.AstronautBlue;
+      return theme.dataColors.Coral;
+    case numberOfCases > 500:
+      return theme.dataColors.WebOrange;
     default:
       return theme.dataColors.NoData;
   }
