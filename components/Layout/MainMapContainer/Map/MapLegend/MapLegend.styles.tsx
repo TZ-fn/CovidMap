94a15ae8bfd1bb31644d1ca91a2df8f3113ca98c
Tomek <tomekz@vp.pl>
2021-10-19 15:19:30 +0200

    Add text and colors to the legend

--- components/Layout/MainMapContainer/Map/MapLegend/MapLegend.styles.tsx
+++ components/Layout/MainMapContainer/Map/MapLegend/MapLegend.styles.tsx
@@ -6,9 +6,9 @@ export const StyledMapLegendContainer = styled.div`
   z-index: 999;
   left: 1em;
   bottom: 1em;
-  width: 15em;
-  height: 20em;
-  padding: 1em;
+  width: 13em;
+  height: min-content;
+  padding: 0.5em;
   background: ${({ theme }) => theme.backgroundLighter};
   border: 1px solid ${({ theme }) => theme.border};
   border-radius: ${theme.misc.borderRadius};
--- components/Layout/MainMapContainer/Map/MapLegend/MapLegend.tsx
+++ components/Layout/MainMapContainer/Map/MapLegend/MapLegend.tsx
@@ -1,12 +1,20 @@
 import { StyledMapLegendContainer } from './MapLegend.styles';
 import DataColorItem from './DataColorItem/DataColorItem';
+import casesToColorMap from 'assets/data/casesToColorMap';
 import theme from 'theme/theme';
 
 export default function Legend(): JSX.Element {
   return (
     <StyledMapLegendContainer>
-      {Object.values(theme.dataColors).map((color) => {
-        return <DataColorItem key={color} color={color}></DataColorItem>;
+      More than:
+      {Object.values(theme.dataColors).map((color, i) => {
+        return (
+          <DataColorItem key={color} color={color}>
+            {color === 'hsl(202, 9%, 61%)'
+              ? 'No data available'
+              : `${Intl.NumberFormat('PL').format(casesToColorMap[i])} cases`}
+          </DataColorItem>
+        );
       })}
     </StyledMapLegendContainer>
   );
