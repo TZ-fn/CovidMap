fc207a63c490dcf64aef536503e8ccd38663e230
Tomek <tomekz@vp.pl>
2021-10-26 14:38:06 +0200

    Remove the obsolete styled types file

--- theme/styled.d.ts
+++
@@ -1,5 +0,0 @@
-import {} from 'styled-components';
-import { ThemeColors } from './themeTypes'; // Import type from above file
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {} // extends the global DefaultTheme with our ThemeType.
-}
