2da140a57ee2b594700c572e724a9d025c4f4401
Tomek <tomekz@vp.pl>
2021-10-26 14:37:31 +0200

    Remove max bounds from the map, add a worldCopyJump property to the Map

--- components/Layout/MainMapContainer/Map/Map.tsx
+++ components/Layout/MainMapContainer/Map/Map.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-import Leaflet from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import { Feature } from 'geojson';
 import allCountries from 'public/geojsonData/allCountries.geo.json';
@@ -14,8 +12,6 @@ interface MapProps {
 }
 
 const Map = ({ covidCasesData }: MapProps): JSX.Element => {
-  const mapRef = useRef(null);
-
   function styleMap(feature: Feature) {
     if (!feature.properties) {
       return;
@@ -32,14 +28,6 @@ const Map = ({ covidCasesData }: MapProps): JSX.Element => {
     };
   }
 
-  const corner1 = Leaflet.latLng(-61, -97);
-  const corner2 = Leaflet.latLng(90, 100);
-  const bounds = Leaflet.latLngBounds(corner1, corner2);
-
-  useEffect(() => {
-    console.log(mapRef);
-  }, []);
-
   return (
     <MapContainer
       center={[45, 0]}
@@ -48,9 +36,7 @@ const Map = ({ covidCasesData }: MapProps): JSX.Element => {
       scrollWheelZoom={false}
       style={{ height: '100%', width: '100%' }}
       maxBoundsViscosity={0.8}
-      maxBounds={bounds}
-      // setMaxBounds={}
-      innerRef={mapRef}
+      worldCopyJump={true}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
