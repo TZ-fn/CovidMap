5ee155b0c3f1654dac201a598a95b97e2aa369a8
Tomek <tomekz@vp.pl>
2021-11-08 15:10:56 +0100

    Add a loading fallback if there is no countryName passed

--- pages/country/[countryName].tsx
+++ pages/country/[countryName].tsx
@@ -3,13 +3,16 @@ import { useRouter } from 'next/router';
 import HeadElement from 'components/Layout/HeadElement/HeadElement';
 import CountryDetailsView from 'views/CountryDetailsView/CountryDetailsView';
 
-export default function Country(): ReactElement {
+export default function Country(): ReactElement | string {
   const router = useRouter();
   const countryName = router.query.countryName;
-  return (
+
+  return countryName ? (
     <>
       <HeadElement title={countryName} />
       <CountryDetailsView countryName={countryName} />
     </>
+  ) : (
+    'Loading ...'
   );
 }
