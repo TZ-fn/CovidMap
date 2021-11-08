f0a17c6d253bdd897e79a01b53f4bac8199c423c
Tomek <tomekz@vp.pl>
2021-11-08 15:12:08 +0100

    Add data fetching for the CountryDetails view

--- views/CountryDetailsView/CountryDetailsView.tsx
+++ views/CountryDetailsView/CountryDetailsView.tsx
@@ -1,19 +1,26 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { StyledCountryDetailsContainer } from './CountryDetailsView.styles';
 import ChartsContainer from './ChartsContainer/ChartsContainer';
 import capitalise from 'utils/capitalise.utils';
+import { fetchData } from 'utils/fetchData.utils';
 
 interface CountryDetailsViewProps {
   countryName: string | string[] | undefined;
 }
 
-export default function CountryDetailsView({
-  countryName = '',
-}: CountryDetailsViewProps): ReactElement {
+export default function CountryDetailsView({ countryName }: CountryDetailsViewProps): ReactElement {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    fetchData(
+      `https://disease.sh/v3/covid-19/historical/${countryName}?lastdays=360`,
+      setChartData,
+    );
+  }, [countryName]);
   return (
     <StyledCountryDetailsContainer>
       <h2>{typeof countryName === 'string' && capitalise(countryName)}</h2>
-      <ChartsContainer />
+      {chartData ? <ChartsContainer /> : 'Loading ...'}
     </StyledCountryDetailsContainer>
   );
 }
