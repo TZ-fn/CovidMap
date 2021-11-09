a1de9e8581c623683ef25c4919430af8f14daeca
Tomek <tomekz@vp.pl>
2021-11-09 14:49:24 +0100

    Remove the obsolete dummy data and customise the tooltip

--- views/CountryDetailsView/Chart/Chart.tsx
+++ views/CountryDetailsView/Chart/Chart.tsx
@@ -13,51 +13,6 @@ import theme, { DataColorsType } from 'theme/theme';
 import { formatChartDataFromAPI } from 'utils/Chart.utils';
 import formatNumberToPolishLocale from 'utils/formatNumberToPolishLocale';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 interface ChartProps {
   fillColor: DataColorsType;
   chartData: any;
@@ -65,7 +20,14 @@ interface ChartProps {
 
 export default function Chart({ fillColor, chartData }: ChartProps): ReactElement {
   const themeColors = useTheme();
-  console.log(formatChartDataFromAPI(chartData));
+
+  function tooltipContent({ active, payload, label }) {
+    if (active && payload && payload.length) {
+      return <p className='label'>{`${label} : ${payload[0].value}`}</p>;
+    }
+    return null;
+  }
+
   return (
     <ResponsiveContainer width='100%' height='100%'>
       <AreaChart
@@ -92,6 +54,7 @@ export default function Chart({ fillColor, chartData }: ChartProps): ReactElemen
           tickFormatter={(tick) => formatNumberToPolishLocale(tick)}
         />
         <Tooltip
+          content={tooltipContent}
           contentStyle={{
             color: themeColors.fontColor,
             backgroundColor: themeColors.backgroundLightest,
