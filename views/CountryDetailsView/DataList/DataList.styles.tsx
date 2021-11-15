6252e14decd1c012bad6f871a41e31cc335ac7a6
Tomek <tomekz@vp.pl>
2021-11-15 14:47:23 +0100

    Add data and styling to the DataList component

--- views/CountryDetailsView/DataList/DataList.styles.tsx
+++ views/CountryDetailsView/DataList/DataList.styles.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 import mainTheme from 'theme/theme';
 
-export const StyledDataList = styled.div`
+export const StyledDataList = styled.ul`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   flex-direction: column;
   background: ${({ theme }) => theme.backgroundLighter};
   border: 1px solid ${({ theme }) => theme.border};
@@ -14,4 +14,26 @@ export const StyledDataList = styled.div`
   width: 33%;
   height: 90%;
   margin-bottom: 1.5rem;
+  padding: 1em 1em;
+  font-size: 1.4em;
+
+  h3,
+  h4 {
+    margin: 0.75rem 0;
+    border-bottom: 1px solid ${mainTheme.dataColors.Coral};
+  }
+
+  h3 {
+    margin: 0 0 1rem 0;
+    font-size: 2.2rem;
+  }
+
+  h4 {
+    font-size: 1.5rem;
+  }
+
+  li {
+    list-style: none;
+    margin: 0.2rem 0;
+  }
 `;
--- views/CountryDetailsView/DataList/DataList.tsx
+++ views/CountryDetailsView/DataList/DataList.tsx
@@ -1,6 +1,27 @@
 import { ReactElement } from 'react';
+import formatObjectNumbersToPolishLocale from 'utils/formatObjectNumbersToPolishLocale.utils';
 import { StyledDataList } from './DataList.styles';
 
-export default function DataList(): ReactElement {
-  return <StyledDataList></StyledDataList>;
+interface DataListProps {
+  countryData: any;
+}
+
+export default function DataList({ countryData }: DataListProps): ReactElement {
+  const formattedCountryData = formatObjectNumbersToPolishLocale(countryData);
+  return (
+    <StyledDataList>
+      <h3>Statistics</h3>
+      <li> Recovered: {formattedCountryData.recovered}</li>
+      <h4>Statistics for today:</h4>
+      <li> New cases today: {formattedCountryData.todayCases}</li>
+      <li> Deaths today: {formattedCountryData.todayDeaths}</li>
+      <li> Active cases for today: {formattedCountryData.active}</li>
+      <li> Recovered today: {formattedCountryData.todayRecovered}</li>
+      <h4>Statistics per one million:</h4>
+      <li>Cases per one million: {formattedCountryData.casesPerOneMillion}</li>
+      <li>Deaths per one million: {formattedCountryData.deathsPerOneMillion}</li>
+      <li>Active per one million: {formattedCountryData.activePerOneMillion}</li>
+      <li>Recovered per one million: {formattedCountryData.recoveredPerOneMillion}</li>
+    </StyledDataList>
+  );
 }
