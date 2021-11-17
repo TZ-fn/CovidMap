95e3677aad0ea4ae83c602a64d17ba0e5e30d742
Tomek <tomekz@vp.pl>
2021-11-17 13:32:48 +0100

    Add a container for the CountryDetails view

---
+++ pages/country/Country.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledCountryViewContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 90vh;
+  min-width: 95vw;
+`;
