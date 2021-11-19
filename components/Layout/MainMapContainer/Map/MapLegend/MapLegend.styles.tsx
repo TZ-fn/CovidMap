dfb637cd8facbe587ceba3af4d392a77d3647a5d
Tomek <tomekz@vp.pl>
2021-11-19 13:08:28 +0100

    Lower the z-index value as low as possible

--- components/Layout/MainMapContainer/Map/MapLegend/MapLegend.styles.tsx
+++ components/Layout/MainMapContainer/Map/MapLegend/MapLegend.styles.tsx
@@ -3,7 +3,7 @@ import theme from 'theme/theme';
 
 export const StyledMapLegendContainer = styled.div`
   position: absolute;
-  z-index: 999;
+  z-index: 400;
   left: 1em;
   bottom: 1em;
   width: 13em;
