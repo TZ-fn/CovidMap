c0aadf029ca734af23dc9b56364b8644b6a5110e
Tomek <tomekz@vp.pl>
2021-11-24 15:40:56 +0100

    Refactor the displaying logic in the component

--- views/CountryDetailsView/CountryDetailsView.tsx
+++ views/CountryDetailsView/CountryDetailsView.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from 'react';
 import { StyledCountryDetailsContainer } from './CountryDetailsView.styles';
-import ChartsContainer from './ChartsContainer/ChartsContainer';
-import capitalise from 'utils/capitalise.utils';
-import LoadingSpinner from 'components/Elements/LoadingSpinner/LoadingSpinner';
 import { useFetch } from 'hooks/useFetch';
+import ErrorContainer from 'components/Elements/ErrorContainer/ErrorContainer';
+import capitalise from 'utils/capitalise.utils';
+import ChartsContainer from './ChartsContainer/ChartsContainer';
+import LoadingSpinner from 'components/Elements/LoadingSpinner/LoadingSpinner';
 
 interface CountryDetailsViewProps {
   countryName: string | string[] | undefined;
@@ -17,11 +18,9 @@ export default function CountryDetailsView({ countryName }: CountryDetailsViewPr
   return (
     <StyledCountryDetailsContainer>
       <h2>{typeof countryName === 'string' && capitalise(countryName)}</h2>
-      {status === 'fetched' ? (
-        <ChartsContainer chartData={chartData} countryName={countryName} />
-      ) : (
-        <LoadingSpinner />
-      )}
+      {error !== null && <ErrorContainer message={error.message} />}
+      {status === 'fetching' && <LoadingSpinner />}
+      {status === 'fetched' && <ChartsContainer chartData={chartData} countryName={countryName} />}
     </StyledCountryDetailsContainer>
   );
 }
