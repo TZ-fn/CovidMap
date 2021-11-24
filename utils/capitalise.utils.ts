b273db0cd795457c708e5be119731ac3bbe17875
Tomek <tomekz@vp.pl>
2021-11-24 15:46:04 +0100

    Add handling of edge cases with USA and UK names

--- utils/capitalise.utils.ts
+++ utils/capitalise.utils.ts
@@ -1,3 +1,6 @@
 export default function capitalise(string: string): string {
+  if (string.toLocaleLowerCase() === 'usa' || string.toLocaleLowerCase() === 'uk') {
+    return string.toUpperCase();
+  }
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
