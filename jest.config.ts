4c30ba49649cbf6e10accf18c95521ba5348bc34
Tomek <tomekz@vp.pl>
2021-12-02 16:01:33 +0100

    Fix an error with @testing-library/jest-dom by reinstalling the package

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,23 @@
+// import type { Config } from '@jest/types';
+// const nextJest = require('next/jest');
+
+// const createJestConfig = nextJest({
+//   dir: './',
+// });
+
+// const customJestConfig: Config.InitialOptions = {
+//   verbose: true,
+//   moduleDirectories: ['node_modules', '.'],
+//   testEnvironment: 'jsdom',
+// };
+// module.exports = createJestConfig(customJestConfig);
+
 import type { Config } from '@jest/types';
-const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({
-  dir: './',
-});
-
-const customJestConfig: Config.InitialOptions = {
+const config: Config.InitialOptions = {
   verbose: true,
   moduleDirectories: ['node_modules', '.'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
-
-module.exports = createJestConfig(customJestConfig);
+export default config;
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from './index';
 
-it('renders the loader where there is no data', async () => {
+it('does not render the rankings where there is not data', () => {
   render(<Home />);
-  console.log(document.body.innerHTML);
-  expect(await screen.findByText(/Covid-19 Map/, {}, { timeout: 3000 })).toBeInTheDocument();
+  expect(screen.queryByText(/Covid-19 cases worldwide/)).not.toBeInTheDocument();
+});
+
+it('renders the main component', () => {
+  render(<Home />);
+  expect(screen.getByRole('main')).toBeInTheDocument();
+});
+
+it('renders the loader when there is no data', () => {
+  render(<Home />);
+  expect(screen.findByTestId(/loading/)).toBeInTheDocument();
 });
