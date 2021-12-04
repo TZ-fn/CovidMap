e3429784b62d94bddd0129fcf95913ffe3cc6001
Tomek <tomekz@vp.pl>
2021-12-04 12:03:25 +0100

    Switch to using next/jest for the testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,15 @@
-// import type { Config } from '@jest/types';
-// const nextJest = require('next/jest');
-
-// const createJestConfig = nextJest({
-//   dir: './',
-// });
-
-// const customJestConfig: Config.InitialOptions = {
-//   verbose: true,
-//   moduleDirectories: ['node_modules', '.'],
-//   testEnvironment: 'jsdom',
-// };
-// module.exports = createJestConfig(customJestConfig);
-
 import type { Config } from '@jest/types';
+const nextJest = require('next/jest');
 
-const config: Config.InitialOptions = {
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig: Config.InitialOptions = {
   verbose: true,
   moduleDirectories: ['node_modules', '.'],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
-export default config;
+
+module.exports = createJestConfig(customJestConfig);
