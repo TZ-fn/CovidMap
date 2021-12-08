4b41d371f9fc03335336e47cf3e0a779fab0ace0
Tomek <tomekz@vp.pl>
2021-12-08 14:31:06 +0100

    Add tests to the SearchBar component

---
+++ __tests__/SearchBar.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import SearchBar from '../components/SearchBar/SearchBar';
+import allCountriesNames from '../assets/data/allCountriesNames';
+
+it('renders the SearchBar', () => {
+  render(<SearchBar countriesNames={allCountriesNames} />);
+  expect(screen.getByPlaceholderText(/Search for a country/)).toBeInTheDocument();
+});
+
+it('renders the results list', () => {
+  render(<SearchBar countriesNames={allCountriesNames} />);
+  const searchBarInput = screen.getByPlaceholderText(/Search for a country/);
+  fireEvent.change(searchBarInput, {
+    target: { value: 'Poland' },
+  });
+  expect(screen.getByText(/Poland/)).toBeInTheDocument();
+});
+
+// it('renders the results list', () => {
+//   render(<SearchBar countriesNames={allCountriesNames} />);
+//   const searchBarInput = screen.getByPlaceholderText(/Search for a country/);
+//   fireEvent.change(searchBarInput, {
+//     target: { value: 'Poland' },
+//   });
+//   const countryResult = screen.getByText(/Poland/);
+//   fireEvent.click(countryResult);
+//   console.log(document.body);
+//   // expect(screen.getByText(/Poland/)).toBeInTheDocument();
+// });
