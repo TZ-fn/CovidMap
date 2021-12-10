cbb39f2b5a9453df6d7c40b27d7ca406dfe15f3f
Tomek <tomekz@vp.pl>
2021-12-10 14:59:28 +0100

    Add more tests to the components

--- __tests__/CountriesList.test.tsx
+++ __tests__/CountriesList.test.tsx
@@ -4,7 +4,7 @@ import CountriesList from '../components/Elements/CountriesList/CountriesList';
 import covidCasesData from '../assets/data/test-mocks/covidCasesData';
 import vaccinesData from '../assets/data/test-mocks/vaccinesData';
 
-it('renders the CountriesList', () => {
+it('renders the CountriesList with correct length', () => {
   render(<CountriesList covidCasesData={covidCasesData} vaccinesData={vaccinesData} />);
   expect(screen.getAllByRole(/listitem/)).toHaveLength(12);
 });
--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '../pages/index';
-
-it('does not render the rankings where there is not data', () => {
-  render(<Home />);
-  expect(screen.queryByText(/Covid-19 cases worldwide/)).not.toBeInTheDocument();
-});
+import covidCasesData from '../assets/data/test-mocks/covidCasesData';
+import vaccinesData from '../assets/data/test-mocks/vaccinesData';
 
 it('renders the main component', () => {
   render(<Home />);
   expect(screen.getByRole('main')).toBeInTheDocument();
 });
 
+it('does not render the rankings where there is not data', () => {
+  render(<Home vaccinesData={null} covidCasesData={null} />);
+  expect(screen.queryByText(/Covid-19 cases worldwide/)).not.toBeInTheDocument();
+});
+
 it('renders the loader when there is no data', () => {
-  render(<Home />);
+  render(<Home vaccinesData={null} covidCasesData={null} />);
   expect(screen.getByTestId('loading')).toBeInTheDocument();
+});
+
+it('renders both rankings when passed correct data', () => {
+  render(<Home vaccinesData={vaccinesData} covidCasesData={covidCasesData} />);
+  expect(screen.queryByText(/Vaccine doses administered/)).toBeInTheDocument();
+  expect(screen.queryByText(/Covid-19 cases worldwide/)).toBeInTheDocument();
 });
