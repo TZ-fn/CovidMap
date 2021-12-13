44df882c29e05eac436ceead451791293d4551f9
Tomek <tomekz@vp.pl>
2021-12-13 15:31:52 +0100

    Change the SearchButton cursor to pointer

--- components/Elements/SearchButton/SearchButton.styles.tsx
+++ components/Elements/SearchButton/SearchButton.styles.tsx
@@ -6,6 +6,7 @@ export const StyledSearchButton = styled(Button)`
   width: 57px;
   height: 57px;
   padding: 0.5em 0.6em 0em 0.6em;
+  cursor: pointer;
 
   & svg {
     fill: ${({ theme }) => theme.fontColor};
