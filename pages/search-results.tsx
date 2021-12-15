39c42ee1b71e9c614fbfdfe8eb701b6b3a6c1572
Tomek <tomekz@vp.pl>
2021-12-15 14:34:22 +0100

    Search Results page works now

--- pages/search-results.tsx
+++ pages/search-results.tsx
@@ -1,5 +1,8 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
+import mainTheme from 'theme/theme';
+import ErrorContainer from 'components/Elements/ErrorContainer/ErrorContainer';
+import { useRouter } from 'next/router';
 
 const StyledContainer = styled.div`
   display: flex;
@@ -20,25 +23,61 @@ const StyledContainer = styled.div`
   }
 `;
 
-interface SearchResultProps {
+const MatchingResultsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 2em;
+  padding: 2em 2em 0 2em;
+  background: ${({ theme }) => theme.backgroundLighter};
+  border: 1px solid ${({ theme }) => theme.border};
+  border-radius: ${mainTheme.misc.borderRadius};
+  box-shadow: ${mainTheme.misc.shadows.mediumElevation};
+`;
+
+interface SearchResultsProps {
   searchQuery: string;
   matchingResults: string[];
 }
 
-export default function SearchResult({
-  searchQuery,
-  matchingResults = ['Cannot find any matching results, please try another query.'],
-}: SearchResultProps): ReactElement {
+function SearchResults({ matchingResults }: SearchResultsProps) {
+  const router = useRouter();
+  const searchQuery = Object.keys(router.query)[0];
+  console.log(Object.keys(router.query)[0]);
+
+  if (!matchingResults) {
+    return (
+      <ErrorContainer
+        message={`Cannot find any matching results for ${searchQuery}, please try another query.`}
+      />
+    );
+  }
+
   return (
-    <StyledContainer>
-      {matchingResults.length > 0 ? 
-      {<p>{`Can't find ${searchQuery}, did you mean:`}</p>
+    <MatchingResultsContainer>
+      <p>{`Can't find ${searchQuery}, did you mean:`}</p>
       <ul>
         {matchingResults.map((result) => {
           return <li key={result}>{result}</li>;
         })}
-      </ul>} :
-      
+      </ul>
+    </MatchingResultsContainer>
+  );
+}
+
+interface SearchResultsPageProps {
+  searchQuery: string;
+  matchingResults: string[];
+}
+
+export default function SearchResultsPage({
+  searchQuery,
+  matchingResults,
+}: SearchResultsPageProps): ReactElement {
+  return (
+    <StyledContainer>
+      <SearchResults searchQuery={searchQuery} matchingResults={matchingResults} />
     </StyledContainer>
   );
 }
