8cdaf81bc5158f77f6c9f2ffe721c4f6d188a9b7
Tomek <tomekz@vp.pl>
2021-12-27 13:47:45 +0100

    Add an Error container to the default Country's page

--- pages/country/index.tsx
+++ pages/country/index.tsx
@@ -1,6 +1,13 @@
+import ErrorContainer from 'components/Elements/ErrorContainer/ErrorContainer';
 import { ReactElement } from 'react';
 import { StyledCountryViewContainer } from './[countryName]';
 
 export default function Country(): ReactElement {
-  return <StyledCountryViewContainer>Please select your country.</StyledCountryViewContainer>;
+  return (
+    <StyledCountryViewContainer>
+      <ErrorContainer
+        message={'Cannot find any matching results for your query, please try another one.'}
+      />
+    </StyledCountryViewContainer>
+  );
 }
