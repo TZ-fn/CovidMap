74d2285df81127820e508fddc5bd6befec1e4b86
Tomek <tomekz@vp.pl>
2022-01-13 15:47:10 +0100

    Add more tests to the Layout component

--- __tests__/Layout.test.tsx
+++ __tests__/Layout.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Layout from '../components/Layout/Layout';
+import { VACCINES_DATA, ALL_COUNTRIES_DATA } from 'assets/data/APIs';
 
 describe('fetches the data', () => {
-  it('fetches from the api 2 times', () => {
+  it('fetches from the correct APIs 2 times', () => {
     render(<Layout>test</Layout>);
     expect(window.fetch).toBeCalledTimes(2);
+    expect(window.fetch).toBeCalledWith(VACCINES_DATA);
+    expect(window.fetch).toBeCalledWith(ALL_COUNTRIES_DATA);
   });
 });
 
