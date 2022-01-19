67761dae795e736c8bd0aeab491de6f8960e4466
Tomek <tomekz@vp.pl>
2022-01-19 15:51:38 +0100

    Trying to mock the useFetch hook, work in progress

--- __tests__/CountryDetailsView.test.tsx
+++ __tests__/CountryDetailsView.test.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import CountryDetailsView from 'views/CountryDetailsView/CountryDetailsView';
-import mockUseFetch from './test-mocks/mockUseFetch';
+import { useFetch } from 'hooks/useFetch';
 
 const countryName = 'Poland';
-
-jest.mock('./test-mocks/mockUseFetch', () => ({
-  useFetch: mockUseFetch('error'),
-}));
+// jest.mock('hooks/useFetch');
 
 it('renders the loader', () => {
   render(<CountryDetailsView countryName={countryName} />);
@@ -15,6 +12,16 @@ it('renders the loader', () => {
 });
 
 it('renders the error message when there is data fetching error', async () => {
+  useFetch.mockReturnValue([null, new Error('An error occurred while fetching the data'), 'error']);
+
   render(<CountryDetailsView countryName={countryName} />);
-  expect(await screen.findByText(/'An error occurred while fetching the data/)).toBeInTheDocument();
+  expect(
+    await screen.findByText(
+      /'An error occurred while fetching the data/,
+      {},
+      {
+        timeout: 2000,
+      },
+    ),
+  ).toBeInTheDocument();
 });
--- __tests__/test-mocks/mockUseFetch.ts
+++ __tests__/test-mocks/mockUseFetch.ts
@@ -1,4 +1,5 @@
 export default function useFetch(input: 'error' | 'fetching'): [null, Error | null, string] {
+  console.log('using mock');
   if (input === 'error') {
     return [null, new Error('An error occurred while fetching the data'), input];
   }
